dec1b62da057e339ccf03064508d322c93096589
Unknown <marcinwos18@gmail.com>
2019-10-11 22:16:39 +0200

    chyba dziala

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import gql from 'graphql-tag';
 
 import Home from './Views/Home';
 import Login from './Views/Login';
+import { useQuery } from '@apollo/react-hooks';
+
+export const me = gql`
+  query me {
+    me {
+      email
+    }
+  }
+`;
 
 const App = () => {
+  const { data, loading, error } = useQuery(me);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return `Error! ${error.message}`;
+
+  if (!data.me && window.location.pathname !== '/login') return <Redirect to="/login" />;
+  if (data.me && window.location.pathname !== '/') return <Redirect to="/" />;
+
   return (
     <Switch>
       <Route path="/login" component={Login} />
@@ -13,4 +31,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default withRouter(App);
