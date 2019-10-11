1dcd192888164111fd2c9efebc2a53876bc73084
CaptainObjective <marcinwos18@gmail.com>
2019-10-11 19:23:27 +0200

    hotfix

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -7,19 +7,9 @@ const Query = {
     if (!ctx.request.userId) {
       return null;
     }
-    const user = await ctx.prisma.query.user({ where: { id: ctx.request.userId } }, info)
+    const user = await ctx.prisma.query.user({ where: { id: ctx.request.userId } }, info);
     return user;
-  },
-
-  recipes: async (parent, args, ctx, info) => {
-    const recipes = await ctx.prisma.query.recipes(args, info)
-    return recipes;
-  },
-  recipe: async (parent, args, ctx, info) => {
-    const recipe = await ctx.prisma.query.recipe(args, info)
-    return recipe;
   }
-
 };
 
 module.exports = Query;
