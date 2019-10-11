f761f5a0290984feb76e6086b854e2f5d1a82eb0
Unknown <marcinwos18@gmail.com>
2019-10-12 01:13:03 +0200

    kupowanie zrobione

--- client/src/Components/Store/index.jsx
+++ client/src/Components/Store/index.jsx
@@ -1,33 +1,56 @@
 import React, { useState } from 'react';
 import { Button, Segment } from 'semantic-ui-react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
 import ToBuy from './toBuy';
 import Bought from './bought';
 
 export const items = [
-    {
-        name: '15 minut dłuższa przerwa',
-        icon: '🍉',
-        description: '',
-        price: 100,
-        bought: false
-    },
-    {
-        name: '1 dzień wolny',
-        icon: '😃',
-        description: '',
-        price: 1000,
-        bought: false
-    },
-    {
-        name: 'Darmowa pizza',
-        icon: '🍕',
-        description: '',
-        price: 200,
-        bought: true
-    }
+  {
+    name: '15 minut dłuższa przerwa',
+    icon: '🍉',
+    description: '',
+    price: 100,
+    bought: false,
+  },
+  {
+    name: '1 dzień wolny',
+    icon: '😃',
+    description: '',
+    price: 1000,
+    bought: false,
+  },
+  {
+    name: 'Darmowa pizza',
+    icon: '🍕',
+    description: '',
+    price: 200,
+    bought: true,
+  },
 ];
 
+const get_store_items = gql`
+  {
+    storeItems {
+      id
+      name
+      desctription
+      price
+      icon
+    }
+    me {
+      id
+      bought_items {
+        id
+      }
+    }
+  }
+`;
+
 const Store = () => {
+  const { data, loading, error } = useQuery(get_store_items);
+
   const [state, setState] = useState({ toBuyView: true, boughtView: false });
 
   const showToBuyBox = () => {
@@ -38,18 +61,22 @@ const Store = () => {
     setState({ toBuyView: false, boughtView: true });
   };
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return `Error! ${error.message}`;
+
+  data.storeItems = data.storeItems.map(el => ({ ...el, bought: data.me.bought_items.some(({ id }) => id === el.id) }));
   return (
     <div className="container">
-    <Segment>
-      <Button onClick={showToBuyBox}>Do kupienia</Button>
-      <Button onClick={showBoughtBox}>Kupione</Button>
-    </Segment>
-    <Segment>
-      {state.toBuyView && <ToBuy items={items}/>}
-      {state.boughtView && <Bought items={items}/>}
-    </Segment>
-  </div>
+      <Segment>
+        <Button onClick={showToBuyBox}>Do kupienia</Button>
+        <Button onClick={showBoughtBox}>Kupione</Button>
+      </Segment>
+      <Segment>
+        {state.toBuyView && <ToBuy me={data.me} items={data.storeItems} />}
+        {state.boughtView && <Bought me={data.me} items={data.storeItems} />}
+      </Segment>
+    </div>
   );
-}
+};
 
 export default Store;
--- client/src/Components/Store/itemToBuy.jsx
+++ client/src/Components/Store/itemToBuy.jsx
@@ -1,37 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Button, Header, Label, Segment } from 'semantic-ui-react';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-const square = { width: 200, height: 200 }
+const square = { width: 200, height: 200 };
+
+const buy_item = gql`
+  mutation($where: UserWhereUniqueInput!, $itemID: ID!) {
+    updateUser(where: $where, data: { bought_items: { connect: { id: $itemID } } }) {
+      id
+      bought_items {
+        id
+      }
+    }
+  }
+`;
 
 const ItemToBuy = props => {
-    const [state, setState] = useState({ message: 'Kup' });
+  console.log(props);
+  const [buyItem, { data }] = useMutation(buy_item, {
+    variables: {
+      where: { id: props.me.id },
+      itemID: props.id,
+    },
+  });
 
-    const onButtonClick = () => {
-        setState({ message: 'Kupiono' });
-    }
+  const [state, setState] = useState({ message: 'Kup' });
 
-    return (    
-        <Segment circular style={square}>
-            <Header as='h4'>
-                <Header.Subheader>
-                    {props.icon}
-                </Header.Subheader>
-                {props.name}
-                <Header.Subheader>
-                    <Button as='div' labelPosition='right' onClick={onButtonClick}>
-                    <Button icon>
-                        {state.message}
-                    </Button>
-                    <Label as='div' basic pointing='left'>
-                        {props.price}
-                    </Label>
-                    </Button>
-                </Header.Subheader>
-            </Header>
-        </Segment>
-   );   
-}
+  const onButtonClick = () => {
+    buyItem();
+    setState({ message: 'Kupiono' });
+  };
+
+  return (
+    <Segment circular style={square}>
+      <Header as="h4">
+        <Header.Subheader>{props.icon}</Header.Subheader>
+        {props.name}
+        <Header.Subheader>
+          <Button as="div" labelPosition="right" onClick={onButtonClick}>
+            <Button icon>{state.message}</Button>
+            <Label as="div" basic pointing="left">
+              {props.price}
+            </Label>
+          </Button>
+        </Header.Subheader>
+      </Header>
+    </Segment>
+  );
+};
 
 export default ItemToBuy;
-
-
