1f776bb4041291ec6e1e26b5ba09813a4b7a89ac
Marcin <marcinzuchowski@wp.pl>
2019-10-12 09:18:07 +0200

    kolory drzewka

--- client/src/Components/Tree/theme.js
+++ client/src/Components/Tree/theme.js
@@ -4,7 +4,7 @@ const theme = {
     heading: {
       font: "Homenaje, Impact, sans-serif"
     },
-    nodeBackgroundColor: "rgba(255,255,255,0.5)",
+    nodeBackgroundColor: "rgba(25,25,25,1)",
     nodeBorderColor: "#54c8ff",
     nodeActiveBackgroundColor: "#54c8ff",
     nodeHoverBorderColor: `linear-gradient(
