177dd2049e81532978deeb5689133821eda2f4fa
Piotr <piotr.bocian.96@gmail.com>
2019-10-12 08:54:26 +0200

    Removed Arrows

--- client/src/Components/Slider.jsx
+++ client/src/Components/Slider.jsx
@@ -4,9 +4,9 @@ import { RenderPromises } from '@apollo/react-hooks';
 
 const Slider = props => {
   const [form, setValues] = useState({
-    first: 'TECHNOLOGY',
-    second: 'TASK',
-    third: 'SOCIAL',
+    first: 'OSIĄGNIĘCIA',
+    second: 'INTEGRACJA',
+    third: 'ZADANIA',
   });
 
   const updateField = e => {
@@ -19,9 +19,7 @@ const Slider = props => {
   return (
     <>
       <div className="Slider">
-        <Button icon inverted color="blue">
-          <Icon name="left arrow" />
-        </Button>
+        
         <Button inverted color="blue" onClick={props.renderTech}>
           {form.first}
         </Button>
@@ -31,9 +29,7 @@ const Slider = props => {
         <Button inverted color="blue" onClick={props.renderSocial}>
           {form.third}
         </Button>
-        <Button icon inverted color="blue">
-          <Icon name="right arrow" />
-        </Button>
+     
       </div>
     </>
   );
